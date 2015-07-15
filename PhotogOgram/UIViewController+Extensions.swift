b22d8af55f5c029c9b709827f318dffc3441e3ec
McLeanBH <mcleanbh@gmail.com>
2015-07-15 17:03:14 -0400

    Implemented alert using UIAlertController class, however --> the extension swift file is currently not working as I would like. I have the alert function written within the AuthViewController.

---
+++ PhotogOgram/UIViewController+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+Extensions.swift
+//  PhotogOgram
+//
+//  Created by Bennett McLean on 7/15/15.
+//  Copyright (c) 2015 Bennett McLean. All rights reserved.
+//
+
+import Foundation
+
+//extension UIViewController
+//{
+//    func showAlert(message: String)
+//    {
+//        self.showAlert("Uh Oh!", message: message)
+//    }
+//
+//    func showAlert(title: String, message: String)
+//    {
+//        var alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+//        alertController.addAction(UIAlertAction(title: "Okay", style: .Default, handler: nil))
+//        self.presentViewController(alertController, animated: true, completion: nil)
+//    }
+//}
+
+
+//func showAlert(message: String)
+//{
+//    var alertController = UIAlertController(title: "Uh Oh!", message: "Sign in failed.", preferredStyle: UIAlertControllerStyle.Alert)
+//    alertController.addAction(UIAlertAction(title: "Okay", style: .Default, handler: nil))
+//    self.presentViewController(alertController, animated: true, completion: nil)
+//}
